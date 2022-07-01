e7eff0aaf39d3a9b4f2b15359b73b4591a3e9b23
CookieCutter4321 <jw9007235@gmail.com>
2022-07-01 14:15:51 -0400

    Added unique ID for form

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,11 +1,15 @@
 function PostForm() {
+	const onClick = () => {
+		console.log("Clicked!")
+	}
+	const unique_id = Math.random().toString(16).slice(2) //generate unique ID
 
 	return (
 		<form>
-			<input name='name' type='text' placeholder='Name' />
-			<input name='title' type='text' placeholder='Title' />
-			<textarea name='text' cols='40' rows='10' placeholder='Text'></textarea>
-			<button type='button'>Submit</button>
+			<input id = {"name_" + unique_id} name='name' type='text' placeholder='Name' />
+			<input id = {"title" + unique_id} name='title' type='text' placeholder='Title' />
+			<textarea id = {"text" + unique_id} name='text' cols='40' rows='10' placeholder='Text'></textarea>
+			<button type='button' onClick = {onClick}>Submit</button>
 		</form>//pass into onClick the name/title. check if valid (has stuff inn it)
 	);
 }
