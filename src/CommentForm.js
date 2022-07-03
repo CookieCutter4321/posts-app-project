501d82097deafc9f102effad6a0b163526b12153
CookieCutter4321 <jw9007235@gmail.com>
2022-07-03 16:36:46 -0400

    Added styling

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -12,7 +12,7 @@ function CommentForm(props) {
 	let unique_id = Math.random().toString(16).slice(2) //generate unique ID for comment
 
 	return (
-		<form id = {unique_form_id}>
+		<form class = "comment_form" id = {unique_form_id}>
 			<input id = {"name" + unique_form_id} name='name' type='text' placeholder='Name' />
 			<textarea id = {"text" + unique_form_id} name='text' cols='30' rows='10' placeholder='Text'></textarea>
 			<button id = {"submit" + unique_form_id} type='button' onClick = {onClick}>Submit</button>
